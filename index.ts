60b852791aa7153d14424dc756f43e1c30aee964
hina2244 <160247675+hina2244@users.noreply.github.com>
2024-04-11 00:21:17 -0700

    push my first commit

---
+++ index.ts
@@ -0,0 +1,95 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let todoList : string[] = [];
+let conditions = true;
+
+while(conditions){
+let answer = await inquirer.prompt(
+    [
+        {
+            name: "select",
+            type: "list",
+            message: "Select an operation",
+            choices: [
+                "Add", "Update", "View",
+                "Delete", "Exit",
+            ]
+        },
+    ]
+);
+ if (answer.select === "Add"){
+    let addTodo = await inquirer.prompt
+    (
+        {
+        name: "todo",
+        type: "input",
+        message: "Add items in the list",
+        validate: function (input) {
+            if(input.trim() == "") {
+                return "Please enter a non-empty items."
+            }
+            return true;
+        }
+        }
+    )
+    if (addTodo.todo.trim() !== ""){
+    todoList.push(addTodo.todo);
+    todoList.forEach(todo => console.log(todo)
+     );
+    }
+ };
+ if (answer.select === "Update"){
+    let UpdateTodo = await inquirer.prompt
+    (
+        {
+            name: "todo",
+            type: "list",
+            message: "Update items in the list",
+            choices: todoList.map(item => item)
+        }
+    )
+    let addTodo = await inquirer.prompt
+    (
+        {
+           name: "todo",
+           type: "input",
+           message: "Add items in the list",
+        }
+    )
+    let newTodo = todoList.filter(val =>val !== UpdateTodo.todo);
+    todoList = [...newTodo,addTodo.todo];
+    todoList.forEach(todo => console.log(todo)
+    );
+ };
+
+ if (answer.select === "View"){
+   console.log("****** TO-DO LIST ******");
+   todoList.forEach(todo => console.log(todo)
+    );
+ };
+
+ if (answer.select === "Delete"){
+    let deleteTodo = await inquirer.prompt
+    (
+        {
+            name: "todo",
+            type: "list",
+            message: "Select items to delete",
+            choices: todoList.map(item => item)
+        }
+    )
+        let newTodo = todoList.filter(val =>val !== deleteTodo.todo);
+        todoList = [...newTodo];
+        todoList.forEach(todo => console.log(todo)
+   );
+ };
+
+ if (answer.select === "Exit"){
+  console.log("Exiting program...");
+  conditions = false;
+ }
+};
+
+
